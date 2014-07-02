1678af4e5093571afe300447e3706669250663f6
Guy Israeli <guy.israeli@gmail.com>
2014-07-02 15:02:32 +0300

    add random

--- src/autumn.js
+++ src/autumn.js
@@ -8,7 +8,7 @@
           container: "map",
           scrollWheenZoom: false,
           center: [51.1788, -1.8262], //stonehenge, that's why
-          maxZoom: 18,
+          maxZoom: 10,
           tiles_url: "http://{s}.tiles.mapbox.com/v3/guyisra.ibee8f32/{z}/{x}/{y}.png",
           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
         },
--- test/demo.js
+++ test/demo.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-$("#map").autumn({center: [1,1]});
+$("#map").autumn({center: [1,1], maxZoom: 18});
 
 
 $("#submit").on("click", function(){
@@ -12,8 +12,22 @@ $("#submit").on("click", function(){
 
 $("#remove").on("click", function(){
   $("#map").autumn("deleteMarkers");
-})
+});
 
+$("#random").on("click", function(){
+  var markers = [];
+  var count = Math.floor(randomCoor(2, 100));
+  for ( var i = 0; i < count; i++) {
+    var coor = [randomCoor(-90, 90), randomCoor(-90,90)];
+    markers.push(coor);
+  }
+  $("#map").autumn("deleteMarkers");
+  $("#map").autumn('addMarker', {markers: markers});
+});
+
+function randomCoor(min,max){
+    return Math.random() * (max - min) + min;
+}
 $("#map").autumn_map().on("click", function(e){
   debugger;
   var ll = e.latlng;
